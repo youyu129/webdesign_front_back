f7229852dca4c6b0cc6205fad388ee00fd8136a2
youyu <129youyu@gmail.com>
2025-02-03 22:45:39 +0800

    調整後台頁尾版權畫面 刪除不需要的檔案

--- backend/footer.php
+++ backend/footer.php
@@ -1,33 +1,29 @@
-<div class="di" style="width:76.5%; margin:2px 0px 0px 0px;">
+<div class="di" style="width:100%; margin:2px 0px 0px 0px;">
     <!--正中央-->
 
     <div style="width:99%; height:87%; margin:auto;" class="mx-auto">
         <h2 class="t cent botli">頁尾版權管理</h2>
         <form method="post" action="./api/update_data.php">
-            <div class="container text-center ">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <p>頁尾版權</p>
-                        <input type="text" name="footer" value="<?=$Footer->find(1)['text'];?>" style="width:80%;"
-                            class="text-center">
-                    </div>
-                </div>
+
+            <div class="input-group mt-5" style="width:80%;margin:auto">
+                <span class="input-group-text" id="basic-addon1">頁尾版權</span>
+                <input class="form-control" type="text" name="acc[]" value="<?=$Footer->find(1)['text'];?>">
             </div>
+
+            </tbody>
+            </table>
+            <table style="margin:auto;margin-top:40px; width:70%;">
+                <tbody>
+                    <tr>
+                        <td class="cent">
+                            <input type="hidden" name="table" value="<?=$do;?>">
+                            <input type="submit" value="修改確定" class="btn btn-outline-info">
+                            <input type="reset" value="重置" class="btn btn-outline-secondary">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
     </div>
-
-    <table style="margin:auto;margin-top:40px; width:70%;">
-        <tbody>
-            <tr>
-                <td class="cent">
-                    <input type="hidden" name="table" value="<?=$do;?>">
-                    <input type="hidden" name="id" value="<?=$Footer->find(1)['id'];?>">
-                    <input type="submit" value="修改確定" class="btn btn-outline-info">
-                    <input type="reset" value="重置" class="btn btn-outline-secondary">
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-    </form>
-</div>
 </div>
