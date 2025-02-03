509572d8ea5e3c05f49f8984a443da0e2b98ad98
youyu <129youyu@gmail.com>
2025-02-04 00:18:32 +0800

    完成後台簡介的更換圖片功能

--- backend/about.php
+++ backend/about.php
@@ -1,38 +1,37 @@
-<div class="di" style="width:76.5%; margin:2px 0px 0px 0px;">
+<div class="di" style="width:100%; margin:2px 0px 0px 0px;">
     <!--正中央-->
 
     <div style="width:99%; height:87%; margin:auto;" class="mx-auto">
         <h2 class="t cent botli">簡介管理</h2>
         <form method="post" action="./api/update_data.php">
-            <div class="mx-auto text-center">
+            <?php
+            $row=$About->find(1);
+            ?>
+            <div class="text-center">
                 <div class="input-group">
                     <span class="input-group-text">簡介</span>
-                    <textarea class="form-control" style="height:30vh;"
-                        aria-label="With textarea"><?=$About->find(1)['text'];?></textarea>
+                    <textarea class="form-control" style="height:25vh;"
+                        aria-label="With textarea"><?=$row['text'];?></textarea>
                 </div>
                 <div class="mt-3">
-                    <img src="upload/sofa.jpg" style="height:30vh;">
-                </div>
-                <div>
-                    <button type="button" class="btn btn-outline-primary"
-                        onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/upload_<?=$do;?>.php&#39;)">更換圖片</button>
+                    <img src="upload/<?=$row['img'];?>" style="height:30vh;">
                 </div>
             </div>
+            <table style="margin:auto;margin-top:40px; width:70%;">
+                <tbody>
+                    <tr>
+                        <td class="cent">
+                            <input type="hidden" name="table" value="<?=$do;?>">
+                            <input type="button" class="btn btn-outline-warning"
+                                onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/upload_<?=$do;?>.php?id=<?=$row['id'];?>&table=<?=$do;?>&#39;)"
+                                value="更換圖片">
+                            <input type="submit" value="修改確定" class="btn btn-outline-info">
+                            <input type="reset" value="重置" class="btn btn-outline-secondary">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
     </div>
-
-    <table style="margin:auto;margin-top:40px; width:70%;">
-        <tbody>
-            <tr>
-                <td class="cent">
-                    <input type="hidden" name="table" value="<?=$do;?>">
-                    <input type="hidden" name="id" value="<?=$About->find(1)['id'];?>">
-                    <input type="submit" value="修改確定" class="btn btn-success">
-                    <input type="reset" value="重置" class="btn btn-secondary">
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-    </form>
-</div>
 </div>
