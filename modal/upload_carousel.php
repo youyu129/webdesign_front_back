39087bb60b2ce8009848653c9d86d5cc4bb3f404
youyu <129youyu@gmail.com>
2025-02-04 00:22:34 +0800

    完成後台輪播圖及環境照片的更換圖片功能

---
+++ modal/upload_carousel.php
@@ -0,0 +1,19 @@
+<h3 class="cent">更新輪播圖</h3>
+<hr>
+<form action="api/update.php" method="post" enctype="multipart/form-data">
+
+    <table>
+        <tr>
+            <td>輪播圖：</td>
+            <td><input type="file" name="img" id="img"></td>
+        </tr>
+
+    </table>
+
+    <div class="cent">
+        <input type="hidden" name="id" value="<?=$_GET['id'];?>">
+        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+        <input type="submit" value="更新">
+        <input type="reset" value="重置">
+    </div>
+</form>
