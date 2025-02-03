efa6eabc83bafa4e7855614afef4b2fd45223850
youyu <129youyu@gmail.com>
2025-02-03 15:17:32 +0800

    新增安怡一頁式網頁

--- index.php
+++ index.php
@@ -10,10 +10,13 @@ include_once "api/db.php";
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <title>卓越科技大學校園資訊系統</title>
+    <title>安怡老人長期照顧中心(養護型)</title>
     <link href="./css/css.css" rel="stylesheet" type="text/css">
     <script src="./js/jquery-1.9.1.min.js"></script>
     <script src="./js/js.js"></script>
+    <style>
+
+    </style>
 </head>
 
 <body>
