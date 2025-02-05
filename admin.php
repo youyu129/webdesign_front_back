f471271bb6a72d869eb4be0c3e6eb2cc1a5ccc00
youyu <129youyu@gmail.com>
2025-02-05 15:14:12 +0800

    更換admin的x為icon

--- admin.php
+++ admin.php
@@ -325,7 +325,7 @@ include_once "api/db.php";
     <div id="cover" style="display:none; ">
         <div id="coverr">
             <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;"
-                onclick="cl(&#39;#cover&#39;)">X</a>
+                onclick="cl(&#39;#cover&#39;)"><i class="bi bi-x-circle"></i></a>
             <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
         </div>
     </div>
